7fa1c985081457e0a857cded45f3b475ef486b20
ActCMS <actcms.com@gmail.com>
2020-10-23 15:50:05 +0700

    feat: update 11ty

--- src/site/css/styles.11ty.js
+++ src/site/css/styles.11ty.js
@@ -4,7 +4,7 @@ const postcss = require('postcss');
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
-const fileName = "main.css";
+const fileName = "styles.css";
 
 
 module.exports = class {
